b3d669f8f66eec4158afd007b7876f5ca340b059
BirdAPI <birdapi@gmail.com>
2011-08-23 19:41:15 -0700

    finds first ssv2 and returns the link for ss and dmi files

--- abgx360.py
+++ abgx360.py
@@ -1,5 +1,7 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
+import sys
+import re
 
 def get_top_search_result(game_name):
     url = "http://abgx360.net/verified.php?f=name&q=%s" % (game_name.replace(" ", "+"))
@@ -14,10 +16,27 @@ def get_top_search_result(game_name):
     href = a["href"]
     link = "http://abgx360.net" + href
     return link
+
+def get_first_ssv2(ss_link):
+    try:
+        html = urllib2.urlopen(ss_link).read()
+    except: return
+    soup = BeautifulSoup(html)
+    evens = soup.findAll(attrs = { "class" : "even" })
+    for even in evens:
+        tds = even.findAll("td");
+        if tds[0]["class"] == "rf" and tds[0].text == "2":
+            ss = "http://abgx360.net" + tds[1].find("a")["href"]
+            dmi = "http://abgx360.net" + tds[2].find("a")["href"]
+            return (ss, dmi)
     
 def main():
-    print get_top_search_result("dirt")
-
+    link = get_top_search_result("dirt")
+    print "Found: " + link
+    (ss, dmi) = get_first_ssv2(link)
+    print "SS: " + ss
+    print "DMI: " + dmi
+    
 if __name__ == "__main__":
     main()
     
