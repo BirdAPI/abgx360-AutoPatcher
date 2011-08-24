56ee4b31bf997ce09f24f4e69c55b11bbf8f5ea4
BirdAPI <birdapi@gmail.com>
2011-08-23 20:07:17 -0700

    some error checking

--- abgx360.py
+++ abgx360.py
@@ -8,29 +8,31 @@ def get_top_search_result(game_name):
     url = "http://abgx360.net/verified.php?f=name&q=%s" % (game_name.replace(" ", "+"))
     try:
         html = urllib2.urlopen(url).read()
-    except: return
-    soup = BeautifulSoup(html)
-    even = soup.find(attrs = { "class" : "even" })
-    tds = even.findAll("td")
-    game = tds[1].text
-    last_td = tds[len(tds) - 1]
-    a = last_td.find('a')
-    href = a["href"]
-    link = "http://abgx360.net" + href
-    return (game, link)
+        soup = BeautifulSoup(html)
+        even = soup.find(attrs = { "class" : "even" })
+        tds = even.findAll("td")
+        game = tds[1].text
+        last_td = tds[len(tds) - 1]
+        a = last_td.find('a')
+        href = a["href"]
+        link = "http://abgx360.net" + href
+        return (game, link)
+    except: 
+        return (None, None)
 
 def get_first_ssv2(ss_link):
     try:
         html = urllib2.urlopen(ss_link).read()
-    except: return
-    soup = BeautifulSoup(html)
-    evens = soup.findAll(attrs = { "class" : "even" })
-    for even in evens:
-        tds = even.findAll("td");
-        if tds[0]["class"] == "rf" and tds[0].text == "2":
-            ss = "http://abgx360.net" + tds[1].find("a")["href"]
-            dmi = "http://abgx360.net" + tds[2].find("a")["href"]
-            return (ss, dmi)
+        soup = BeautifulSoup(html)
+        evens = soup.findAll(attrs = { "class" : "even" })
+        for even in evens:
+            tds = even.findAll("td");
+            if tds[0]["class"] == "rf" and tds[0].text == "2":
+                ss = "http://abgx360.net" + tds[1].find("a")["href"]
+                dmi = "http://abgx360.net" + tds[2].find("a")["href"]
+                return (ss, dmi)
+    except: 
+        return (None, None)
 
 def download_file(link, dest_dir):
     u = urllib2.urlopen(link)
@@ -54,12 +56,18 @@ def ensure_dir_exists(dir):
 def main():
     if len(sys.argv) == 2:
         (game, link) = get_top_search_result(sys.argv[1])
-        print game + ": " + link
-        (ss, dmi) = get_first_ssv2(link)
-        print "SS: " + ss
-        print "DMI: " + dmi
-        download_file(ss, "Patches/" + game)
-        download_file(dmi, "Patches/" + game)
+        if game is not None:
+            print game + ": " + link
+            (ss, dmi) = get_first_ssv2(link)
+            if ss is not None and dmi is not None:
+                print "SS: " + ss
+                print "DMI: " + dmi
+                download_file(ss, "Patches/" + game)
+                download_file(dmi, "Patches/" + game)
+            else:
+                print "No SSv2 Patches Found!"
+        else:
+            print "No Results: " + sys.argv[1]
     else:
         print "Usage: abgx360.py game_name"
     
